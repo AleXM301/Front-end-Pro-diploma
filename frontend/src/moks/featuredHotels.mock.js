306283d584319983a8f30ccc5aadb6b91bb4690f
vlad <sashkamarichok@gmail.com>
2025-06-27 15:45:23 +0300

    refactor: add mock data

---
+++ frontend/src/moks/featuredHotels.mock.js
@@ -0,0 +1,38 @@
+import the_grand_resort from "@assets/the_grand_resort.jpg";
+
+const featuredHotelsMock = [
+    {
+        id: 1,
+        img: the_grand_resort,
+        name: 'The Grand Resort',
+        country: 'Maldives',
+        rating: '4.5',
+        cell: '450'
+
+    }, {
+        id: 2,
+        img: the_grand_resort,
+        name: 'The Grand Resort',
+        country: 'Maldives',
+        rating: '4.5',
+        cell: '450'
+
+    }, {
+        id: 3,
+        img: the_grand_resort,
+        name: 'The Grand Resort',
+        country: 'Maldives',
+        rating: '4.5',
+        cell: '450'
+
+    }, {
+        id: 4,
+        img: the_grand_resort,
+        name: 'The Grand Resort',
+        country: 'Maldives',
+        rating: '4.5',
+        cell: '450'
+
+    },
+]
+export default  featuredHotelsMock
